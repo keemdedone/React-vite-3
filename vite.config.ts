3fb9173ac5f3b74b814666b992c3dd501c201e71
keemdedone <komchan447@gmail.com>
2023-06-04 15:06:06 +0700

    fix css & add base url to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/React-vite-3",
   plugins: [react()],
   server: {
     port: 3000,
